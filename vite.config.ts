c4cc3db6abff7b68725ea6edb03606a3019c20e7
Saurabh <iamsaurabhtyagi@gmail.com>
2024-09-14 23:43:53 +0530

    Port updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost', // This binds to all available IPs on your EC2 instance
+    port: 3000,
+  }
 })
